ebe758e291923f0bb19c82c85deda58bbae45a86
rygo6 <rygo6@me.com>
2019-08-25 20:47:14 -0700

    initial commit

---
+++ Scripts/RectTransformTweens.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace GeoTetra.GTTween
+{
+    public static class RectTransformTweens
+    {
+        public static IEnumerator ToLocalPosition(
+            RectTransform rectTransform, 
+            Vector2 targetPosition, 
+            float multiplier,
+            AnimationCurve easeCurve, 
+            Action onFinish)
+        {
+            float time = 0;
+            float easeTime = 0;
+            Vector2 startPosition = rectTransform.localPosition;
+
+            while (time < 1)
+            {
+                time += Time.deltaTime * multiplier;
+                easeTime = easeCurve.Evaluate(time);
+                rectTransform.localPosition = Vector2.Lerp(startPosition, targetPosition, easeTime);
+                yield return null;
+            }
+
+            rectTransform.localPosition = targetPosition;
+
+            if (onFinish != null)
+            {
+                onFinish();
+            }
+        }
+    }
+}
